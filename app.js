304bdb6904d115d7c42f302db441aa05e570ee0f
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-07 01:16:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,38 +3,77 @@ var port = process.env.PORT || 3000,
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+    function sendFile(filename, contentType, callback) {
+        fs.readFile(filename, function(error, data) {
+            if (error) {
+                console.log(error);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('Something does not work well');
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.write(data);
             }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
             res.end();
+            if (callback) {
+                callback(error);
+            }
         });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'text/javascript'});
-        res.write(html);
-        res.end();
     }
-});
+    
+    const server = http.createServer(function(req, res) {
+        if (req.url === "/index.js") {
+            sendFile('index.js', 'text/javascript');
+        } else if (req.url === "/") {
+            sendFile('index.html', 'text/html');
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
+    })
+    
+    server.listen(port, function(error) {
+        if (error) {
+            console.log('Something went wrong' + error)
+        } else {
+            console.log('Everything is working fine on port ' + port)
+        }
+    })
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+
+
+// var log = function(entry) {
+//     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+// };
+
+
+// var server = http.createServer(function (req, res) {
+//     if (req.method === 'POST') {
+//         var body = '';
+
+//         req.on('data', function(chunk) {
+//             body += chunk;
+//         });
+
+//         req.on('end', function() {
+//             if (req.url === '/') {
+//                 log('Received message: ' + body);
+//             } else if (req.url = '/scheduled') {
+//                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+//             }
+
+//             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+//             res.end();
+//         });
+//     } else {
+//         res.writeHead(200, { 'Content-Type': 'text/javascript'});
+//         res.write(html);
+//         res.end();
+//     }
+// });
+
+// // Listen on port 3000, IP defaults to 127.0.0.1
+// server.listen(port);
+
+// // Put a friendly message on the terminal
+// console.log('Server running at http://127.0.0.1:' + port + '/');
