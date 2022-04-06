381824ae2ec63a59bf60f77dcfcd95c4d324521a
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-07 02:02:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,25 +40,72 @@ var port = process.env.PORT || 3000,
 // };
 
 
-var server = http.createServer(function (req, res) {
-    if (req.url === '/') {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    } else if (req.url.endsWith('.js')) {
+var server = http.createServer(function (request, response) {
 
-        // html = fs.readFileSync('index.html');
-        fs.readFile(path.join(__dirname, req.url), function (err,data){
-            if (err) {
-              res.writeHead(404);
-              res.end(JSON.stringify(err));
-              return;
-            }
-            res.writeHead(200);
-            res.setHeader("Content-Type", "application/javascript");
-            res.end(data);
-        });
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
     }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+    // if (req.url === '/') {
+    //     res.writeHead(200);
+    //     res.write(html);
+    //     res.end();
+    // } else if (req.url.endsWith('.js')) {
+
+    //     // html = fs.readFileSync('index.html');
+    //     fs.readFile(path.join(__dirname, req.url), function (err,data){
+    //         if (err) {
+    //           res.writeHead(404);
+    //           res.end(JSON.stringify(err));
+    //           return;
+    //         }
+    //         res.writeHead(200);
+    //         res.setHeader("Content-Type", "application/javascript");
+    //         res.end(data);
+    //     });
+    // }
     // fs.readFile(path.join(__dirname, req.url), function (err,data){
     //     // if (err) {
     //     //     res.writeHead(404);
