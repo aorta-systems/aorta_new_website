8805c8a820c3d0e512b63c4d84a8e9090ed54841
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-14 23:42:52 +0300

    next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  headerLogoSrc = "assets/images/logo_aorta.png";
   title = 'aorta_new_website';
 }
