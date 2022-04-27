4041885ed446b86fc14dd17d3afd10c48ad1cc15
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-27 12:53:05 +0300

    add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ModalContactUsComponent } from './modal-contact-us/modal-contact-us.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ServicePageComponent } from './service-page/service-page.component';
+import { ServicesPageComponent } from './services-page/services-page.component';
 // import { ServicesPageComponent } from './services-page/services-page.component';
 
 @NgModule({
@@ -27,6 +29,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HomePageComponent,
     ObserveVisibilityDirective,
     ModalContactUsComponent,
+    ServicePageComponent,
+    ServicesPageComponent,
   ],
   imports: [
     BrowserModule,
