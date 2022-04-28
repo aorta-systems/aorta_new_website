17248f96063101288a585042efb1791344866cec
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-28 12:47:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 
 const express = require("express");
-const bodyParser = require("body-parser")
   
 // New app using express module
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
   
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "./index.html");
