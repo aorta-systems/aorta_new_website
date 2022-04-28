7821fb68ea4a936e47e494336cec630068347b8f
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-28 18:48:01 +0300

    Revert "try express"
    This reverts commit b4fb5d2e5c5cb4e71a1e302ef7d33a2aede4cca6.

--- app.js
+++ app.js
@@ -1,117 +1,59 @@
+var port = process.env.PORT || 3000;
+var http = require('http');
+var fs = require('fs');
+var html = fs.readFileSync('index.html');
+var path = require('path');
 
-var express = require("express");
-// Use body-parser
-var bodyParser = require("body-parser");
+var server = http.createServer(function (request, response) {
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
 
-// Create new instance of the express server
-var app = express();
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
 
-// Define the JSON parser as a default way 
-// to consume and produce data through the 
-// exposed APIs
-app.use(bodyParser.json());
+    var contentType = mimeTypes[extname] || undefined;
 
-// Create link to Angular build directory
-// The `ng build` command will save the result
-// under the `dist` folder.
-var distDir = __dirname + "/index.html";
-app.use(express.static(distDir));
+    if (contentType == undefined) {
+        filePath = './index.html';
+        contentType = 'text/html';
+    }
 
-// Init the server
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 });
 
-/*  "/api/status"
- *   GET: Get server status
- *   PS: it's just an example, not mandatory
- */
-app.get("/api/status", function (req, res) {
-    res.status(200).json({ status: "UP" });
-});
-
-
-app.get("/api/aa", function (req, res) {
-    res.status(200).json({ dir: distDir });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var port = process.env.PORT || 3000;
-// var http = require('http');
-// var fs = require('fs');
-// var html = fs.readFileSync('index.html');
-// var path = require('path');
-
-// var server = http.createServer(function (request, response) {
-//     var filePath = '.' + request.url;
-//     if (filePath == './') {
-//         filePath = './index.html';
-//     }
-
-//     var extname = String(path.extname(filePath)).toLowerCase();
-//     var mimeTypes = {
-//         '.js': 'text/javascript',
-//         '.css': 'text/css',
-//         '.json': 'application/json',
-//         '.png': 'image/png',
-//         '.jpg': 'image/jpg',
-//         '.gif': 'image/gif',
-//         '.svg': 'image/svg+xml',
-//         '.wav': 'audio/wav',
-//         '.mp4': 'video/mp4',
-//         '.woff': 'application/font-woff',
-//         '.ttf': 'application/font-ttf',
-//         '.eot': 'application/vnd.ms-fontobject',
-//         '.otf': 'application/font-otf',
-//         '.wasm': 'application/wasm'
-//     };
-
-//     var contentType = mimeTypes[extname] || undefined;
-
-//     if (contentType == undefined) {
-//         filePath = './index.html';
-//         contentType = 'text/html';
-//     }
-
-//     fs.readFile(filePath, function(error, content) {
-//         if (error) {
-//             if(error.code == 'ENOENT') {
-//                 fs.readFile('./404.html', function(error, content) {
-//                     response.writeHead(404, { 'Content-Type': 'text/html' });
-//                     response.end(content, 'utf-8');
-//                 });
-//             }
-//             else {
-//                 response.writeHead(500);
-//                 response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-//             }
-//         }
-//         else {
-//             response.writeHead(200, { 'Content-Type': contentType });
-//             response.end(content, 'utf-8');
-//         }
-//     });
-// });
-
-// // Listen on port 3000, IP defaults to 127.0.0.1
-// server.listen(port);
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
