e3c660e1cd730d97e32e43cd32a0c54bb95f388e
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-04-28 12:42:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
 }));
   
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "./index.html");
 });
   
 app.post("/", function(req, res) {
