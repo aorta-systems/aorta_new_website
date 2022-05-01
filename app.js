361b9e2405f8bd0ef277d48788ec69bd4aedaa26
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-05-02 01:38:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+var port = process.env.PORT || 3000;
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors({ origin: "*" }));
-app.use(bodyParser.json());
+// app.use(cors({ origin: "*" }));
+// app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("The server started on port 3000 !!!!!!");
+app.listen(port, () => {
+  console.log("The server started 3000 !!!!!!");
 });
 
 app.get("/", (req, res) => {
@@ -16,56 +17,6 @@ app.get("/", (req, res) => {
     "<h1 style='text-align: center'>Wellcome to FunOfHeuristic <br><br>😃👻😃👻😃👻😃👻😃</h1>"
   );
 });
-
-app.post("/sendmail", (req, res) => {
-  console.log("request came");
-  let user = req.body;
-  sendMail(user, info => {
-    console.log(`The mail has beed send 😃 and the id is ${info.messageId}`);
-    res.send(info);
-  });
-});
-
-async function sendMail(user, callback) {
-  // create reusable transporter object using the default SMTP transport
-//   let transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: details.email,
-//       pass: details.password
-//     }
-//   });
-
-
-    let transporter = nodemailer.createTransport({
-      host: "smtp.zoho.com",
-      port: 465,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "aleksei@aortasystems.com",
-        pass: "WBPxSEC7dmHP"
-      }
-    });
-
-  let mailOptions = {
-    from: '"Fun Of Heuristic"<example.gimail.com>', // sender address
-    to: user.email, // list of receivers
-    subject: "Wellcome to Fun Of Heuristic 👻", // Subject line
-    html: `<h1>Hi ${user.name}</h1><br>
-    <h4>Thanks for joining us</h4>`
-  };
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail(mailOptions);
-
-  callback(info);
-}
-
-
-
-
 
 
 
