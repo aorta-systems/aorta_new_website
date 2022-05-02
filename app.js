067bfd09bf29bf4dc7f8d79a08f00291f927b1cb
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-05-02 04:56:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.post('/sendmail', (req, res) => {
     console.log("request came");
     let email = req.body;
 
-
     // res.send(email);
     sendMail(email, info => {
+        console.log("email");
         res.send(info);
     });
 });
@@ -28,146 +28,33 @@ app.listen(port, () => {
     console.log("The server started on port:" + port);
 });
 
-
-
-
-async function sendMail(user, callback) {
-    // create reusable transporter object using the default SMTP transport
+async function sendMail(email, callback) {
     let transporter = nodemailer.createTransport({
       host: "smtp.zoho.com",
       port: 465,
-      secure: false, // true for 465, false for other ports
+      secure: true,
       auth: {
         user: "aleksei@aortasystems.com",
-        pass: "WBPxSEC7dmHP"
+        pass: "Aa461033"
       }
     });
 
-//WBPxSEC7dmHP
-
     let mailOptions = {
-      from: '"AORTA SITE MESSAGE"<example.gimail.com>', // sender address
+      from: '"Aorta SITE"<aleksei@aortasystems.com>', // sender address
       to: "aleksei@aortasystems.com", // list of receivers
       subject: "AORTA SITE MESSAGE", // Subject line
-      html: `<h1>Hi</h1><br>
-      <h4>Thanks for joining us</h4>`
+      html: `
+        <h3>User name: </h3><br>
+        <h4>${email.name}</h4><br>
+        <h3>Email</h3><br>
+        <h4>${email.email}</h4><br>
+        <h3>Phone</h3><br>
+        <h4>${email.phone}</h4><br>
+        <h3>Message</h3><br>
+        <h4>${email.message}</h4><br>
+      `
     };
-  
-    // send mail with defined transport object
     let info = await transporter.sendMail(mailOptions);
   
     callback(info);
 }
-
-
-// app.use(cors({ origin: "*" }));
-// app.use(bodyParser.json());
-
-
-// app.server.get('*.*', express.static('./', {maxAge: '1y'}));
-
-// app.all('*', function (req, res) {
-//     res.status(200).sendFile('./index.html', {root: './'});
-// });
-
-
-
-
-// var express = require("express");
-// var port = process.env.PORT || 3000;
-// var http = require('http');
-// var fs = require('fs');
-// var html = fs.readFileSync('index.html');
-// var path = require('path');
-// var nodemailer = require("nodemailer");
-
-
-// const app = 
-
-// async function sendMail(user, callback) {
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//       host: "smtp.zoho.com",
-//       port: 465,
-//       secure: false, // true for 465, false for other ports
-//       auth: {
-//         user: "aleksei@aortasystems.com",
-//         pass: "WBPxSEC7dmHP"
-//       }
-//     });
-
-// //WBPxSEC7dmHP
-
-//     let mailOptions = {
-//       from: '"AORTA SITE MESSAGE"<example.gimail.com>', // sender address
-//       to: "aleksei@aortasystems.com", // list of receivers
-//       subject: "AORTA SITE MESSAGE", // Subject line
-//       html: `<h1>Hi</h1><br>
-//       <h4>Thanks for joining us</h4>`
-//     };
-  
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail(mailOptions);
-  
-//     callback(info);
-// }
-
-// var server = http.createServer(function (request, response) {
-//     if (request.method === 'POST' &&  request.url === '/sendEmail') {
-//         sendMail(email, info => {
-//             res.send(info);
-//         });
-//     }
-
-//     var filePath = '.' + request.url;
-//     if (filePath == './') {
-//         filePath = './index.html';
-//     }
-
-//     var extname = String(path.extname(filePath)).toLowerCase();
-//     var mimeTypes = {
-//         '.js': 'text/javascript',
-//         '.css': 'text/css',
-//         '.json': 'application/json',
-//         '.png': 'image/png',
-//         '.jpg': 'image/jpg',
-//         '.gif': 'image/gif',
-//         '.svg': 'image/svg+xml',
-//         '.wav': 'audio/wav',
-//         '.mp4': 'video/mp4',
-//         '.woff': 'application/font-woff',
-//         '.ttf': 'application/font-ttf',
-//         '.eot': 'application/vnd.ms-fontobject',
-//         '.otf': 'application/font-otf',
-//         '.wasm': 'application/wasm'
-//     };
-    
-//     var contentType = mimeTypes[extname] || undefined;
-
-//     if (contentType == undefined) {
-//         filePath = './index.html';
-//         contentType = 'text/html';
-//     }
-
-//     fs.readFile(filePath, function(error, content) {
-//         if (error) {
-//             if(error.code == 'ENOENT') {
-//                 fs.readFile('./404.html', function(error, content) {
-//                     response.writeHead(404, { 'Content-Type': 'text/html' });
-//                     response.end(content, 'utf-8');
-//                 });
-//             }
-//             else {
-//                 response.writeHead(500);
-//                 response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-//             }
-//         }
-//         else {
-//             response.writeHead(200, { 'Content-Type': contentType });
-//             response.end(content, 'utf-8');
-//         }
-//     });
-// });
-
-// // Listen on port 3000, IP defaults to 127.0.0.1
-// server.listen(port);
