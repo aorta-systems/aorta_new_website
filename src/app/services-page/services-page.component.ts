1dfd6d92fc1871b5f4a76864ffa25b1e2ef54421
Aorta Systems <aorta-systems@users.noreply.github.com>
2022-05-03 02:22:50 +0300

    services up

--- src/app/services-page/services-page.component.ts
+++ src/app/services-page/services-page.component.ts
@@ -6,21 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-page.component.css']
 })
 export class ServicesPageComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
   
   onVisible(element: HTMLElement) {
-    if (!element.classList.contains("active")) {
-      element.classList.add("active");
+    var serviceCardElement = <HTMLInputElement>element.closest('.service-card');
+    if (!serviceCardElement.classList.contains("active")) {
+      serviceCardElement.classList.add("active");
     }
   }
 
   onInVisible(element: HTMLElement) {
-    element.classList.remove("active");
+    var serviceCardElement = <HTMLInputElement>element.closest('.service-card');
+    var serviceCardContainerElement = <HTMLInputElement>element.closest('.service-card-container');
+    serviceCardElement.classList.remove("active");
+    serviceCardContainerElement.classList.remove("highlight-service-card");
   }
 
 
+  moveToService(sectionName: String) {
+    var allServices = <HTMLCollectionOf<Element>>document.getElementsByClassName('highlight-service-card') || [];
+    Array.from(allServices).forEach(function (element) {
+      element.classList.remove("highlight-service-card");
+    });
+  
+    var serviceCardElement = <HTMLInputElement>document.getElementById(`${sectionName}`);
+    serviceCardElement.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});;
+    serviceCardElement.classList.add("highlight-service-card");
+  }
 }
